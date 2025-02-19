38d91c2a0d256ed65dbf13d1239814a9b7fc9390
Leihan Chen <leihan.chen@torontomu.ca>
2025-02-18 19:52:28 -0500

    sqlite upgrade deployment

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# Solving pysqlite issue with streamlit
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import pyperclip
 import streamlit as st
 import streamlit_nested_layout
